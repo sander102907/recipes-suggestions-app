72d6b960f296977d2698ab949ee8935053dc130c
sander102907 <s.d.bruin@hotmail.com>
2022-07-14 22:37:58 +0200

    Cron fix (#40)
    * change cron package
    
    * change cron package
    
    * change cron package
    
    * change cron package
    
    * change cron package

--- backend/src/cronjobs/recipe.cronjobs.ts
+++ backend/src/cronjobs/recipe.cronjobs.ts
@@ -1,20 +1,14 @@
-import cron from "node-cron";
+import schedule from "node-schedule";
 import { RecipeHelper } from "../utils/recipeHelper";
 
 export class RecipeCronjobs {
-
-    constructor() {
-    }
-
-    // Default cronTime: every monday at 3:00 AM
-    public setRecipeSuggestions(cronTime = '0 0 3 * * 1') {
+    // Default cronTime: every monday at 2:00 AM UTC
+    public setRecipeSuggestions(cronTime = '0 0 2 * * 1') {
         // Synchronize all ingredients
-        cron.schedule(cronTime, async () => {
+        schedule.scheduleJob('set recipe suggestions', cronTime, async () => {
             console.log("running weekly cron schedule to update recipe suggestions");
 
             await RecipeHelper.setRecipeSuggestions();
-        }, {
-            timezone: "Europe/Amsterdam"
         });
     }
 }
