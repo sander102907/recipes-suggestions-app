e518918f9c4af16dff2dd7f4c48adcf54fc08c14
Sander <s.d.bruin@hotmail.com>
2022-08-31 20:09:06 +0200

    fix cronjobs

--- backend/src/cronjobs/recipe.cronjobs.ts
+++ backend/src/cronjobs/recipe.cronjobs.ts
@@ -1,4 +1,6 @@
 import schedule from "node-schedule";
+import { freemem, loadavg, totalmem } from "os";
+import FileHelper from "../utils/fileHelper";
 import { RecipeHelper } from "../utils/recipeHelper";
 
 export class RecipeCronjobs {
@@ -8,7 +10,9 @@ export class RecipeCronjobs {
         schedule.scheduleJob('set recipe suggestions', cronTime, async () => {
             console.log("running weekly cron schedule to update recipe suggestions");
 
+            FileHelper.appendFile("logs/log.txt", `${new Date().toUTCString()} - running weekly cron schedule to update recipe suggestions. Memory usage: ${(freemem() / 10e6).toFixed(1)}MB/${(totalmem() / 10e6).toFixed(1)}MB. Load average (5min): ${loadavg()[1]} \n`)
             await RecipeHelper.setRecipeSuggestions();
+
         });
     }
 }
